185b5548ef69061f0d69f374a5fdf0b752b87e73
izhammulya <79027973+izhammulya@users.noreply.github.com>
2025-09-29 11:42:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -436,7 +436,7 @@ def generate_notulen_with_ai(sentences, api_key):
         # model = genai.GenerativeModel("gemini-2.5-flash")
 
         # model = genai.GenerativeModel("models/gemini-2.5-flash-lite") 
-        model = model.GenerativeModel("gemini-2.5-flash-lite-preview-09-2025")
+        model = genai.GenerativeModel("gemini-2.5-flash-lite-preview-09-2025")
         
         # REFINED PROMPT with strong emphasis on professional, non-sensitive content
         prompt = f"""
