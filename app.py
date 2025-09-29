f6fea66df6e4f2a8303b4627bdb6774ba62665e8
izhammulya <79027973+izhammulya@users.noreply.github.com>
2025-09-29 10:23:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def generate_notulen_with_ai(sentences, api_key):
         
         # Initialize model
         # model = genai.GenerativeModel("models/gemini-1.5-flash-8b-latest")
-        model = genai.GenerativeModel("models/gemini-2.5-pro-preview-06-05")
+        model = genai.GenerativeModel("models/gemini-2.5-flash")
         
         # EXACT PROMPT - DO NOT CHANGE
         prompt = f"""
